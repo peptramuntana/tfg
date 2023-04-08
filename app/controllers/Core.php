7b7a4149c5b93a20347875cfdf055d4aae2bf68a
Pep <vilarospro@gmail.com>
2023-04-08 20:25:53 +0200

    Restructuring the architecture

--- app/controllers/Core.php
+++ app/controllers/Core.php
@@ -1,10 +1,24 @@
 <?php
 
 /* Map the loaded URL from Client */
-
-
 class Core {
-    // protected $currentController = 'Pages';   
+    protected $currentController = 'Pages';
+    protected $currentMethod = 'index';
+    protected $params = [];
+
+    // Constructor
+    public function __construct() {
+        $url = $this->getUrl();
+    }
+
+
+    public function getUrl() {
+        //It gets the URL, starting from "index.php"
+        echo "Hello world";
+        var_dump($_GET);
+
+        echo $_GET['url'];
+    }
 }
 
 ?>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+    //Import the Core class
+    require_once('app/controllers/indexControllers.php');
+
+    //Create a new instance of the Core class
+    $init = new Core;
+
+?>
