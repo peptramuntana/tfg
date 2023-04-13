f801fd52ac2ecc2b0ec9385cac52779a558c86e9
peptramuntana <vilarospro@gmail.com>
2023-04-14 01:18:31 +0200

    Clean the  code

--- index.php
+++ index.php
@@ -1,18 +1,16 @@
 <?php
-    //Import the Model and Controller
+    //Import all the Function Querys of the Model
     require_once('app/Models/indexModels.php');
+    //Import the logic of the Controller
     require_once('app/controllers/indexControllers.php');
     
-
-    //Call to the method which returns the URL
-    echo "hello world";
-    echo "<br>";
-    echo $_SERVER['REQUEST_URI'];
-    //defined("LANG") or define("LANG", "es");
-    include 'app/views/indexViews.php';
+    include('app/views/indexViews.php');
     //include 'app/views/'.Core::dameVista($url).'.php';
+    
+   
     echo "<pre>";
     print_r($url);
+    // print_r($output);
     echo '</pre>';
     
 ?>
