c707df0f76589f28ea2cdde980fb4af3f2572c59
peptramuntana <vilarospro@gmail.com>
2023-04-14 01:20:29 +0200

    Modify the MVC indexes

--- app/controllers/indexControllers.php
+++ app/controllers/indexControllers.php
@@ -1,8 +1,29 @@
 <?php
-    require('Core.php');
+    require_once('Core.php');
+    
+    // Get the URL
     $url = Core::getUrl();
-    defined("VISTA") or define('VISTA', Core::dameVista($url));
+    // Define the global variables from the URL
+    Core::urlVariables($url);
 
+
+    print_r($url);
+
+    echo "<br>";
+    print_r("The language is ". LANG);
+    echo "<br>";
+
+    print_r("La vista is ".VIEW .'.php');
+    echo "<br>";
+
+    $langId = Database::getLangId(LANG);
+    echo "<br>";
+
+    echo "<br>";
+
+    print_r($langId);
+    // $view = Database::getView();
+    print_r($view);
     // Cargamos si tenemos el controlador específico de la vista
-    include __DIR__ . '/'.Core::dameVista($url).'.php'; // REVISAR
+    // include __DIR__ . '/'.Core::dameVista($url).'.php'; // REVISAR
 ?>
--- app/models/indexModels.php
+++ app/models/indexModels.php
@@ -1,3 +1,3 @@
 <?php
-    include_once('app/models/db.php');
+    require_once('app/models/db.php');
 ?>
