c34039ab5bd163b260d8ab0c000623458b7b340d
peptramuntana <vilarospro@gmail.com>
2023-04-14 01:18:18 +0200

    Modify a variable

--- app/views/indexViews.php
+++ app/views/indexViews.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="<?php echo LANG_TAG; ?>">
+<html lang="<?php echo LANG; ?>">
 <head>
     <?php include __DIR__ . '/modules/header.php'; ?>
 </head>
 <body>
-    <?php include __DIR__ .'/'.VISTA.'.php'; ?>
+   <!-- <?php include __DIR__ .'/'.VIEW.'.php'; ?> -->
 </body>
 </html>
