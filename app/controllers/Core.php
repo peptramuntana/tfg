cf26a34894ce045146cde71aa35acb8def8895c1
peptramuntana <vilarospro@gmail.com>
2023-04-14 00:10:40 +0200

    Create a new function which collect data and create the global variables

--- app/controllers/Core.php
+++ app/controllers/Core.php
@@ -2,6 +2,23 @@
 
 class Core {
 
+    public function defineGlobalVariables($array) {
+        // Get the first parameter of the URL
+        $firstParam = array_shift($array);
+        // Check if the first parameter is a language
+        if(strlen($firstParam) == 2)
+        {
+            // Define language set by the user
+            define("LANG", $firstParam);
+            define("VISTA", $array[0]);
+        }
+        else{
+            // Define the default language
+            define("LANG", "es");
+            define("VISTA", $firstParam);
+        }
+    }
+
     static function getUrl() {
 
         // Define variables 
@@ -18,33 +35,24 @@ class Core {
         // Remove the first index of the array (empty)
         $url = array_filter($url);
 
-        // Get the first parameter of the URL
-        $firstParam = array_shift($url);
-        // Check if the first parameter is a language
-        if(strlen($firstParam) == 2)
-        {
-            // Define language set by the user
-            define("LANG", $firstParam);
-        }
-        else{
-            // Define the default language
-            define("LANG", "es");
-        }
-
-        // Add the first param to the array
-        $output[] = $firstParam;
-
         // Add the rest of the params to the array
         foreach ($url as $key => $value) {
             $output[] = $value;
         }
-        
+
+        // We create a new instance of the class to use the method
+        $classInstance = new Core();
+        // We call the method to define the global variables
+        $classInstance ->defineGlobalVariables($output);
+
+
         return $output;
     }
 
 
+
     // Pasamos Array con URLs (ej: proyectos/categoria/filtro2) 
-    static function dameVista($url)
+    static function getView($url)
     {
         if(count($url) == 1){
             // HAgo query y me devuelve datos de la vista   
