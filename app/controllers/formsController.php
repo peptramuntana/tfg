e397f02451e3309bdfe894ed3f12cc286835e90a
peptramuntana <vilarospro@gmail.com>
2023-05-02 22:53:28 +0200

    Fix Data catch in $_POST

--- app/controllers/formsController.php
+++ app/controllers/formsController.php
@@ -2,6 +2,8 @@
     require_once('../models/db.php');
     require_once('Core.php');
 
+    define("SYSTEM_LANG", $_POST['lang']);
+
     if (isset($_POST['login'])) {
         Core::checkLogin();
     }
@@ -11,14 +13,28 @@
     }
 
     if (isset($_POST['update'])) {
+        
         $projectID = $_POST['projectID'];
         $title = $_POST['title'];
         $description = $_POST['description'];
     
         $images = [];
         foreach ($_POST as $key => $value) {
-            if (strpos($key, 'image-url-') === 0) {
-                $counter = substr($key, strlen('image-url-'));
+            echo $key;
+            echo '<br>';
+            echo $value;
+            echo '<br>';
+            // if (substr($key, 0, -1) === 'image-url-') {
+            //     $counter = substr($value, -1);
+            //     $image = [
+            //         'url' => $_POST['image-url-' . $counter],
+            //         'alt' => $_POST['image-alt-' . $counter],
+            //         'title' => $_POST['image-title-' . $counter]
+            //     ];
+            //     $images[] = $image;
+            // }
+            if (strpos($value, 'image-url-') !== false) {
+                $counter = substr($key, -1);
                 $image = [
                     'url' => $_POST['image-url-' . $counter],
                     'alt' => $_POST['image-alt-' . $counter],
@@ -28,13 +44,21 @@
             }
         }
         
+        print_r("images[] -->".var_dump($images));
+        echo '<br>';
+
+        foreach ($_POST as $post => $value) {
+            var_dump($post. ' --> ' .$value);
+            echo '<br>';
+        }
+        
+        echo '<br>';
         print_r("projectID -->$projectID");
         echo '<br>';
         print_r("title -->$title");
         echo '<br>';        
         print_r("description -->$description");
         echo '<br>';
-        var_dump($images);
-        echo '<br>';
+        
     }
 ?>
