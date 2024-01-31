be705f3654c3149a8e25d25c3e8bd44e8ec93070
peptramuntana <vilarospro@gmail.com>
2024-01-31 22:01:11 +0100

    Implement Title, Metadescriptions, and OG.

--- app/views/modules/head.php
+++ app/views/modules/head.php
@@ -18,14 +18,53 @@
 <meta name="msapplication-TileColor" content="#ffffff">
 <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
 <meta name="theme-color" content="#8ABE3B">
+<meta property="article:author" content="Vilkeer Projects">
+<meta property="article:published_time" content="2024-01-31 11:04:50">
+<meta property="article:modified_time" content="2024-01-31 14:15:50">
+<meta property="og:locale" content="es-ES">
+<meta property="og:type" content="website">
+<meta property="og:url" content="https://www.vilkeerprojects.com/es/">
+<meta property="og:site_name" content="Vilkeer Projects">
+<meta property="og:image:width" content="1200">
+<meta property="og:image:height" content="630">
+<meta property="og:image" content="https://www.vilkeerprojects.com/es/public/images/og/opengraph.jpg">
+<meta name="twitter:image:src" content="https://www.vilkeerprojects.com/es/public/images/og/opengraph.jpg">
+<meta name="twitter:card" content="summary">
+<meta name="twitter:site" content="Vilkeer Projects">
 <?php
-if (SYSTEM_VIEW ==  "projects" || SYSTEM_VIEW == "home") {
-    echo '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css">';
-    echo '<script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>';
+$textsData = Database::getTexts();
+
+if (SYSTEM_VIEW == "home") {
+    echo '<meta property="og:title" content="' . $textsData['head-title-home'] . '">';
+    echo '<meta property="og:description" content="' . $textsData['head-description-home'] . '">';
+    echo '<meta name="twitter:description" content="' . $textsData['head-description-home'] . '">';
+    echo '<meta name="twitter:title" content="' . $textsData['head-title-home'] . '">';
+    echo '<title>' . $textsData['head-title-home'] . '</title>';
+    echo '<meta name="description" content="' . $textsData['head-description-home'] . '">';
+}
+
+if (SYSTEM_VIEW == "projects") {
+    echo '<meta property="og:title" content="' . $textsData['head-title-projects'] . '">';
+    echo '<meta property="og:description" content="' . $textsData['head-description-projects'] . '">';
+    echo '<meta name="twitter:description" content="' . $textsData['head-description-projects'] . '">';
+    echo '<meta name="twitter:title" content="' . $textsData['head-title-projects'] . '">';
+    echo '<title>' . $textsData['head-title-projects'] . '</title>';
+    echo '<meta name="description" content="' . $textsData['head-description-projects'] . '">';
 }
 
 if (SYSTEM_VIEW == "contact") {
+    echo '<meta property="og:title" content="' . $textsData['head-title-contact'] . '">';
+    echo '<meta property="og:description" content="' . $textsData['head-description-contact'] . '">';
+    echo '<meta name="twitter:description" content="' . $textsData['head-description-contact'] . '">';
+    echo '<meta name="twitter:title" content="' . $textsData['head-title-contact'] . '">';
+    echo '<title>' . $textsData['head-title-contact'] . '</title>';
+    echo '<meta name="description" content="' . $textsData['head-description-contact'] . '">';
     echo '<script src="https://www.google.com/recaptcha/api.js?render=6Le-V2ApAAAAAEsVtCSTQUPf6DzT6AfaBe2fRpZc"></script>';
+}
+
+if (SYSTEM_VIEW ==  "projects" || SYSTEM_VIEW == "home") {
+    echo '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css">';
+    echo '<script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>';
 }
 
 if (SYSTEM_VIEW == "administrator") {
@@ -37,4 +76,3 @@ if (SYSTEM_VIEW == "administrator") {
 
 <link rel="stylesheet" href="../../../public/templates/css/styles.css">
 <script src="../../../public/templates/js/scripts.js"></script>
-<title><?php echo str_replace("-", " ", ucwords(SYSTEM_URL)) ?></title>
